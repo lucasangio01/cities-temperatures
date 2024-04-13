0c8a0805177a506ff11d9afc7de5f13689236194
lucasangio01 <146674237+lucasangio01@users.noreply.github.com>
2024-04-13 21:54:38 +0200

    update

--- project/utils.py
+++ project/utils.py
@@ -6,12 +6,12 @@ import pandas as pd
 # The following class contains the imported data and data that I used frequently in other functions.
 
 class Data:
-    path = "C:\\Users\sangi\Desktop\Info progetto python\Datasets"
-    tempByCity = pd.read_csv(path + "\GlobalLandTemperaturesByCity.csv").dropna().reset_index(drop=True)
-    tempByMajorCity = pd.read_csv(path + "\GlobalLandTemperaturesByMajorCity.csv").dropna().reset_index(drop=True)
-    majorCities = pd.read_csv(path + "\majorCities.csv", index_col=0)
-    cities = pd.read_csv(path + "\cities.csv", index_col=0)
-    cities_old = pd.read_csv(path + "\cities_old.csv", index_col=0, on_bad_lines="skip")
+    path = "https://raw.githubusercontent.com/lucasangio01/cities-temperatures/main/datasets"
+    tempByCity = pd.read_csv("C:/Users/sangi/Downloads/GlobalLandTemperaturesByCity/GlobalLandTemperaturesByCity.csv").dropna().reset_index(drop=True)
+    tempByMajorCity = pd.read_csv(path + "/GlobalLandTemperaturesByMajorCity.csv").dropna().reset_index(drop=True)
+    majorCities = pd.read_csv(path + "/majorCities.csv", index_col=0)
+    cities = pd.read_csv(path + "/cities.csv", index_col=0)
+    cities_old = pd.read_csv(path + "/cities_old.csv", index_col=0, on_bad_lines="skip")
     months = {"01": 'January', '02': 'February', '03': 'March', '04': 'April', '05': 'May', '06': 'June', '07': 'July',
               '08': 'August', '09': 'September', '10': 'October', '11': 'November', '12': 'December'}
 
